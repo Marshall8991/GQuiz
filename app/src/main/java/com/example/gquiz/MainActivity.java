8559fa7c32e576253170dc4c1ffb8ecae347ff5f
Vyacheslav <vyacheslav.zhukov.1998@gmail.com>
2020-03-21 02:01:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gquiz/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.gquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int currentQuestionIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        questionView = findViewById(R.id.question);
+
+        applyCurrentQuestion();
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // move to next question
+                if (currentQuestionIndex == mQuestionBank.length - 1) {
+                    currentQuestionIndex = 0;
+                } else {
+                    currentQuestionIndex++;
+                }
+
+                // apply question
+                applyCurrentQuestion();
+            }
+        });
+    }
+
+    private void applyCurrentQuestion() {
+        questionView.setText(getCurrentQuestion().getQuestionResId());
+    }
+
+    private Question getCurrentQuestion() {
+        return mQuestionBank[currentQuestionIndex];
+    }
+
+    private void onAnswerSelected(boolean currentAnswer) {
+        boolean wasTheAnswerCorrect = currentAnswer == getCurrentQuestion().getCorrectAnswer();
+
+        showToast(wasTheAnswerCorrect ? R.string.correct_toast : R.string.incorrect_toast);
+    }
+
+    private void showToast(int textId) {
+        Toast.makeText(MainActivity.this, textId, Toast.LENGTH_SHORT).show();
+    }
+}
